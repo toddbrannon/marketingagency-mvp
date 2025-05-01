0b89542e18e13941cb68be94f145ba2cf4f84ce6
toddbrannon <toddqbrannon@gmail.com>
2025-04-30 21:57:26 -0500

    edits to calendar.tsx

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,38 @@
 import * as React from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, NavigationProps } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
 export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+
+function CustomNavigation({ nextMonth, previousMonth }: NavigationProps) {
+  return (
+    <div className="space-x-1 flex items-center">
+      <button
+        type="button"
+        onClick={() => previousMonth && previousMonth()}
+        className={cn(
+          buttonVariants({ variant: 'outline' }),
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute left-1'
+        )}
+      >
+        <ChevronLeftIcon className="h-4 w-4" />
+      </button>
+      <button
+        type="button"
+        onClick={() => nextMonth && nextMonth()}
+        className={cn(
+          buttonVariants({ variant: 'outline' }),
+          'h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100 absolute right-1'
+        )}
+      >
+        <ChevronRightIcon className="h-4 w-4" />
+      </button>
+    </div>
+  );
+}
 
 function Calendar({
   className,
@@ -58,8 +85,7 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
+        Navigation: CustomNavigation,
       }}
       {...props}
     />
