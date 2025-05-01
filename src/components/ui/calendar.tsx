eec9014002aaa063a540ddecd1744c2462e20d5f
toddbrannon <toddqbrannon@gmail.com>
2025-04-30 22:16:44 -0500

    make it better

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@radix-ui/react-icons';
-import { DayPicker } from 'react-day-picker';
+import { DayPicker, type DayPickerProps } from 'react-day-picker';
 
 import { cn } from '@/lib/utils';
 import { buttonVariants } from '@/components/ui/button';
 
-export type CalendarProps = React.ComponentProps<typeof DayPicker>;
+export type CalendarProps = DayPickerProps; // ✅ This is the change
 
 function Calendar({
   className,
